09e6c2247deb9a0b3f16fa5e194d2dcc8ce99344
Sarah Berg <sberg@us.ci.org>
2023-12-02 11:56:27 -0600

    move reused code to its own function, optimized scenario check

--- 2023/day02/index.js
+++ 2023/day02/index.js
@@ -10,57 +10,46 @@ const ELF_SCENARIO = {
     blue: 14,
 };
 
+/* CHALLENGE: PART ONE */
 function part1(games){
     //answer: 2545
-
     const passSum = games.reduce((sum, game, index) => {
-        const [, gameData] = game.split(": ");
-        const subGames = gameData.split("; ");
-    
-        const maxCounts = subGames.reduce((subGameMax, subGame) => {
-            const subGameData = subGame.split(', ');
-            subGameData.forEach((subGameDataColor) => {
-                const [count, color] = subGameDataColor.split(' ');
-                const countValue = parseInt(count);
-                subGameMax[color] = Math.max(subGameMax[color], countValue);
-            });
-            return subGameMax;
-        }, { red: 0, green: 0, blue: 0 });
-    
-        if (maxCounts.red <= ELF_SCENARIO.red && maxCounts.green <= ELF_SCENARIO.green && maxCounts.blue <= ELF_SCENARIO.blue) {
+        const gameColorMax = getGameColorMax(game)
+        if (Object.entries(gameColorMax).every(([color, count]) => count <= ELF_SCENARIO[color])) {
             return sum + (index + 1);
         }
-    
         return sum;
     }, 0);
-
     return passSum;
 }
 
+/* CHALLENGE: PART TWO */
 function part2(games){
     //answer: 78111
-
     const powerSum = games.reduce((sum, game, index) => {
-        const [, gameData] = game.split(": ");
-        const subGames = gameData.split("; ");
-    
-        const maxCounts = subGames.reduce((subGameMax, subGame) => {
-            const subGameData = subGame.split(', ');
-            subGameData.forEach((subGameDataColor) => {
-                const [count, color] = subGameDataColor.split(' ');
-                const countValue = parseInt(count);
-                subGameMax[color] = Math.max(subGameMax[color], countValue);
-            });
-            return subGameMax;
-        }, { red: 0, green: 0, blue: 0 });
-
-        return sum + (maxCounts.red * maxCounts.green * maxCounts.blue);
+        const gameColorMax = getGameColorMax(game)
+        return sum + (gameColorMax.red * gameColorMax.green * gameColorMax.blue);
     }, 0);
-
     return powerSum;
 }
 
-// part1(games)
+/* FUNCTION : getGameColorMax */
+function getGameColorMax(game){
+    const [, gameData] = game.split(": ");
+    const subGames = gameData.split("; ");
+
+    const maxCounts = subGames.reduce((subGameMax, subGame) => {
+        const subGameData = subGame.split(', ');
+        subGameData.forEach((subGameDataColor) => {
+            const [count, color] = subGameDataColor.split(' ');
+            const countValue = parseInt(count);
+            subGameMax[color] = Math.max(subGameMax[color], countValue);
+        });
+        return subGameMax;
+    }, { red: 0, green: 0, blue: 0 });
+
+    return maxCounts;
+}
 
 console.log(part1(games),'part1');
 console.log(part2(games),'part2?');
