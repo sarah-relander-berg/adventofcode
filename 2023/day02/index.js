82bb1fe634394ebb2ffb8eaaec7341cdc9f15ee3
Sarah Berg <sberg@us.ci.org>
2023-12-02 11:40:57 -0600

    Unveiling the mysteries of Snow Island's cube-filled bags! ❄️🎲

--- 2023/day02/index.js
+++ 2023/day02/index.js
@@ -1,20 +1,69 @@
 import fs from 'fs';
 
-const lines = fs.readFileSync('./example.txt','utf-8').trim().split('\n');
-// const lines = fs.readFileSync('./data.txt','utf-8').trim().split('\n');
+// const games = fs.readFileSync('./example.txt','utf-8').trim().split('\n');
+const games = fs.readFileSync('./data.txt','utf-8').trim().split('\n');
+// console.log(games,'games')
 
-// console.log(lines,'lines')
+const ELF_SCENARIO = {
+    red: 12,
+    green: 13,
+    blue: 14,
+};
 
-function part1(lines){
+function part1(games){
+    //answer: 2545
 
-    return lines;
-}
-
-function part2(lines){
+    const passSum = games.reduce((sum, game, index) => {
+        const [, gameData] = game.split(": ");
+        const subGames = gameData.split("; ");
     
-    return lines;
+        const maxCounts = subGames.reduce((subGameMax, subGame) => {
+            const subGameData = subGame.split(', ');
+            subGameData.forEach((subGameDataColor) => {
+                const [count, color] = subGameDataColor.split(' ');
+                const countValue = parseInt(count);
+                subGameMax[color] = Math.max(subGameMax[color], countValue);
+            });
+            return subGameMax;
+        }, { red: 0, green: 0, blue: 0 });
+    
+        if (maxCounts.red <= ELF_SCENARIO.red && maxCounts.green <= ELF_SCENARIO.green && maxCounts.blue <= ELF_SCENARIO.blue) {
+            return sum + (index + 1);
+        }
+    
+        return sum;
+    }, 0);
+
+    return passSum;
 }
 
-console.log(part1(lines),'part1');
-console.log(part2(lines),'part2');
+function part2(games){
+    //answer: 78111
+
+    const powerSum = games.reduce((sum, game, index) => {
+        const [, gameData] = game.split(": ");
+        const subGames = gameData.split("; ");
+    
+        const maxCounts = subGames.reduce((subGameMax, subGame) => {
+            const subGameData = subGame.split(', ');
+            subGameData.forEach((subGameDataColor) => {
+                const [count, color] = subGameDataColor.split(' ');
+                const countValue = parseInt(count);
+                subGameMax[color] = Math.max(subGameMax[color], countValue);
+            });
+            return subGameMax;
+        }, { red: 0, green: 0, blue: 0 });
+
+        return sum + (maxCounts.red * maxCounts.green * maxCounts.blue);
+    }, 0);
+
+    return powerSum;
+}
+
+// part1(games)
+
+console.log(part1(games),'part1');
+console.log(part2(games),'part2?');
   
+
+
